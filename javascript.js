78e03cc3f85b4518e61e0c957e091ec47e8eba5a
Justin <justinrkinsman@gmail.com>
2022-07-18 11:37:30 -0300

    Add custom error message to country input

--- javascript.js
+++ javascript.js
@@ -1,3 +1,29 @@
+/*function validate(inputID){
+    const input = document.getElementById(inputID);
+    //const validityState = input.validity;
+    console.log(input.checkValidity())
+/*    if (validityState.valueMissing) {
+        input.setCustomValidity('Please fill out this input');
+    }else if (validityState.tooShort){
+        input.setCustomValidity('not long enough');
+    }else{
+        input.setCustomValidity('');
+    }
+
+    input.reportValidity();
+}
+
+const country = document.getElementById('country');
+country.addEventListener('input', validate('country') //{
+  /*  if (!country.checkValidity()){
+        country.setCustomValidity("please fill out country name")
+        country.reportValidity()
+    }else{
+        country.setCustomValidity("")
+        country.reportValidity()
+    }
+})*/
+
 const email = document.getElementById('email');
 email.addEventListener('input', function(event) {
     if (email.validity.typeMismatch) {
@@ -8,3 +34,40 @@ email.addEventListener('input', function(event) {
     }
 })
 
+/*country.addEventListener('input', () => {
+    country.setCustomValidity('');
+    country.checkValidity();
+});
+
+country.addEventListener('invalid', () => {
+    if(country.value === '') {
+        country.setCustomValidity('Enter your nationality');
+    }else{
+        country.setCustomValidity('Country name must be at least 2 characters')
+    }
+})*/
+
+/*function validate(inputID){
+    const input = document.getElementById(inputID);
+    const validityState = input.validity;
+    /*if (validityState.valueMissing) {
+        input.setCustomValidity('Please fill out this input');
+    }else if (validityState.tooShort){
+        input.setCustomValidity('not long enough');
+    }else{
+        input.setCustomValidity('');
+    }
+    input.reportValidity();
+}*/
+
+const nationality = document.getElementById('country');
+nationality.addEventListener('input', () => {
+    const validityState = nationality.validity
+    if (validityState.tooShort){
+        nationality.setCustomValidity('Please input at least 4 characters')
+        nationality.reportValidity()
+    }else{
+        nationality.setCustomValidity('')
+        nationality.reportValidity()
+    }
+})
