2c08e0eafd8159a8caa4b9847d625f8f40e72727
Justin <justinrkinsman@gmail.com>
2022-07-18 11:56:47 -0300

    Add custom error message to zipcode input

--- javascript.js
+++ javascript.js
@@ -1,31 +1,3 @@
-function validate(inputID){
-    const input = document.getElementById(inputID);
-    //const validityState = input.validity;
-    console.log(input.checkValidity())
-    if (validityState.valueMissing) {
-        input.setCustomValidity('Please fill out this input');
-    }else if (validityState.tooShort){
-        input.setCustomValidity('not long enough');
-    }else{
-        input.setCustomValidity('');
-    }
-
-    input.reportValidity();
-}
-
-
-
-/*const country = document.getElementById('country');
-country.addEventListener('input', validate('country') //{
-  /*  if (!country.checkValidity()){
-        country.setCustomValidity("please fill out country name")
-        country.reportValidity()
-    }else{
-        country.setCustomValidity("")
-        country.reportValidity()
-    }
-})*/
-
 const email = document.getElementById('email');
 email.addEventListener('input', function(event) {
     if (email.validity.typeMismatch) {
@@ -35,32 +7,6 @@ email.addEventListener('input', function(event) {
         email.setCustomValidity('')
     }
 })
-
-/*country.addEventListener('input', () => {
-    country.setCustomValidity('');
-    country.checkValidity();
-});
-
-country.addEventListener('invalid', () => {
-    if(country.value === '') {
-        country.setCustomValidity('Enter your nationality');
-    }else{
-        country.setCustomValidity('Country name must be at least 2 characters')
-    }
-})*/
-
-/*function validate(inputID){
-    const input = document.getElementById(inputID);
-    const validityState = input.validity;
-    /*if (validityState.valueMissing) {
-        input.setCustomValidity('Please fill out this input');
-    }else if (validityState.tooShort){
-        input.setCustomValidity('not long enough');
-    }else{
-        input.setCustomValidity('');
-    }
-    input.reportValidity();
-}*/
 
 const nationality = document.getElementById('country');
 nationality.addEventListener('input', () => {
@@ -74,5 +20,20 @@ nationality.addEventListener('input', () => {
     }else{
         nationality.setCustomValidity('')
         nationality.reportValidity()
+    }
+})
+
+const zip = document.getElementById('zipCode');
+zip.addEventListener('input', () => {
+    const validityState = zip.validity
+    if (validityState.patternMismatch){
+        zip.setCustomValidity('Please use numbers only')
+        zip.reportValidity()
+    }else if (validityState.tooShort){
+        zip.setCustomValidity('Please input at least 5 characters')
+        zip.reportValidity()    
+    }else{
+        zip.setCustomValidity('');
+        zip.reportValidity()
     }
 })
